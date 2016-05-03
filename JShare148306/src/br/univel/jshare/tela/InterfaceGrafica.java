dc72a1e59f5caf3a565e3acc50640eeb50243563
Fabricio Cornelius <Fabricio Cornelius>
2016-05-02 22:21:26 -0300

    Algumas coisas mais tela

--- JShare148306/src/br/univel/jshare/tela/InterfaceGrafica.java
+++ JShare148306/src/br/univel/jshare/tela/InterfaceGrafica.java
@@ -27,6 +27,9 @@ import javax.swing.JLabel;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JTable;
 
 public class InterfaceGrafica extends JFrame implements IServer{
 
@@ -53,7 +56,7 @@ public class InterfaceGrafica extends JFrame implements IServer{
 	 */
 	public InterfaceGrafica() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 529, 360);
+		setBounds(100, 100, 552, 397);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -66,19 +69,91 @@ public class InterfaceGrafica extends JFrame implements IServer{
 		panel.setToolTipText("");
 		tabbedPane.addTab("Cliente", null, panel, null);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[]{29, 100, 61, 82, 0, 0, 0, 35, 40, 52, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 47, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
+		
+		JLabel lblEndereoIp = new JLabel("Endere\u00E7o IP:");
+		GridBagConstraints gbc_lblEndereoIp = new GridBagConstraints();
+		gbc_lblEndereoIp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblEndereoIp.anchor = GridBagConstraints.WEST;
+		gbc_lblEndereoIp.gridx = 1;
+		gbc_lblEndereoIp.gridy = 1;
+		panel.add(lblEndereoIp, gbc_lblEndereoIp);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 4;
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 1;
+		panel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblPorta_1 = new JLabel("Porta:");
+		GridBagConstraints gbc_lblPorta_1 = new GridBagConstraints();
+		gbc_lblPorta_1.anchor = GridBagConstraints.EAST;
+		gbc_lblPorta_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPorta_1.gridx = 6;
+		gbc_lblPorta_1.gridy = 1;
+		panel.add(lblPorta_1, gbc_lblPorta_1);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 7;
+		gbc_textField_1.gridy = 1;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnConectar = new JButton("Conectar");
+		GridBagConstraints gbc_btnConectar = new GridBagConstraints();
+		gbc_btnConectar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnConectar.gridx = 9;
+		gbc_btnConectar.gridy = 1;
+		panel.add(btnConectar, gbc_btnConectar);
+		
+		JLabel lblListaDeArquivos = new JLabel("Arquivos:");
+		GridBagConstraints gbc_lblListaDeArquivos = new GridBagConstraints();
+		gbc_lblListaDeArquivos.anchor = GridBagConstraints.WEST;
+		gbc_lblListaDeArquivos.insets = new Insets(0, 0, 5, 5);
+		gbc_lblListaDeArquivos.gridx = 1;
+		gbc_lblListaDeArquivos.gridy = 3;
+		panel.add(lblListaDeArquivos, gbc_lblListaDeArquivos);
+		
+		table = new JTable();
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.gridheight = 4;
+		gbc_table.gridwidth = 8;
+		gbc_table.insets = new Insets(0, 0, 0, 5);
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 1;
+		gbc_table.gridy = 4;
+		panel.add(table, gbc_table);
+		
+		JButton btnDownload = new JButton("Download");
+		btnDownload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnDownload = new GridBagConstraints();
+		gbc_btnDownload.anchor = GridBagConstraints.WEST;
+		gbc_btnDownload.gridwidth = 2;
+		gbc_btnDownload.gridx = 9;
+		gbc_btnDownload.gridy = 7;
+		panel.add(btnDownload, gbc_btnDownload);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Servidor", null, panel_1, null);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0};
+		gbl_panel_1.columnWidths = new int[]{0, 93, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
 		
 		JLabel lblIpLocal = new JLabel("IP Local:");
@@ -98,7 +173,7 @@ public class InterfaceGrafica extends JFrame implements IServer{
 		
 		JLabel lblPastaCompartilhada = new JLabel("Pasta Compartilhada");
 		GridBagConstraints gbc_lblPastaCompartilhada = new GridBagConstraints();
-		gbc_lblPastaCompartilhada.insets = new Insets(0, 0, 0, 5);
+		gbc_lblPastaCompartilhada.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPastaCompartilhada.anchor = GridBagConstraints.WEST;
 		gbc_lblPastaCompartilhada.gridx = 1;
 		gbc_lblPastaCompartilhada.gridy = 2;
@@ -110,16 +185,41 @@ public class InterfaceGrafica extends JFrame implements IServer{
 			}
 		});
 		GridBagConstraints gbc_btnIniciarServio = new GridBagConstraints();
-		gbc_btnIniciarServio.insets = new Insets(0, 0, 0, 5);
+		gbc_btnIniciarServio.insets = new Insets(0, 0, 5, 5);
 		gbc_btnIniciarServio.gridx = 8;
 		gbc_btnIniciarServio.gridy = 2;
 		panel_1.add(btnIniciarServio, gbc_btnIniciarServio);
 		
 		JButton btnParaServio = new JButton("Para Servi\u00E7o");
 		GridBagConstraints gbc_btnParaServio = new GridBagConstraints();
+		gbc_btnParaServio.insets = new Insets(0, 0, 5, 0);
 		gbc_btnParaServio.gridx = 9;
 		gbc_btnParaServio.gridy = 2;
 		panel_1.add(btnParaServio, gbc_btnParaServio);
+		
+		JButton button = new JButton("...");
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.anchor = GridBagConstraints.WEST;
+		gbc_button.insets = new Insets(0, 0, 5, 5);
+		gbc_button.gridx = 1;
+		gbc_button.gridy = 3;
+		panel_1.add(button, gbc_button);
+		
+		JLabel lblPessoasConectadas = new JLabel("Pessoas Conectadas:");
+		GridBagConstraints gbc_lblPessoasConectadas = new GridBagConstraints();
+		gbc_lblPessoasConectadas.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPessoasConectadas.gridx = 1;
+		gbc_lblPessoasConectadas.gridy = 6;
+		panel_1.add(lblPessoasConectadas, gbc_lblPessoasConectadas);
+		
+		JList list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridwidth = 7;
+		gbc_list.insets = new Insets(0, 0, 0, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 1;
+		gbc_list.gridy = 7;
+		panel_1.add(list, gbc_list);
 	}
 	
 	
@@ -128,6 +228,9 @@ public class InterfaceGrafica extends JFrame implements IServer{
 	private IServer servidor;
 	
 	private Registry registry;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTable table;
 
 	@Override
 	public void registrarCliente(Cliente c) throws RemoteException {
